c06a9a1731e7f33c121066b655d0a7169ba64eda
vera zeng <zengheng810@gmail.com>
2016-02-28 13:51:22 +0800

    Initial Commit

---
+++ BooksSearcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BooksSearcher
+//
+//  Created by Heng Zeng on 2/28/16.
+//  Copyright © 2016 verazeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BooksSearcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BooksSearcher
+//
+//  Created by Heng Zeng on 2/28/16.
+//  Copyright © 2016 verazeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
